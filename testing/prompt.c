93cee74c741ea05f1d0e6c077fe1cef6d78033ec
xefigueroa <xefigueroa@github.com>
2021-04-11 02:50:29 +0000

    shell at least runs. Needs improvement

---
+++ testing/prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+void print_prompt1(void)
+{
+	fprintf(stderr, "$ ");
+}
+
+void print_prompt2(void)
+{
+	fprintf(stderr, "> ");
+}
--- testing/shell.h
+++ testing/shell.h
@@ -7,4 +7,9 @@
 #include <stdlib.h>
 #include <errno.h>
 
+void print_prompt1(void);
+void print_prompt2(void);
+
+char *read_cmd(void);
+
 #endif
