d8fc743083c4c07c4fbefa250c1ccd518d7ad7d5
JosePabloBonilla <2750@holbertonschool.com>
2021-04-11 04:01:51 +0000

    update header

--- testing/shell.h
+++ testing/shell.h
@@ -6,9 +6,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void print_prompt1(void);
 void print_prompt2(void);
+void _puts(char *str);
+int _strlen(const char *s);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+char _strcmp(char *s1, char *s2);
+int _putchar(char c);
 
 char *read_cmd(void);
 
