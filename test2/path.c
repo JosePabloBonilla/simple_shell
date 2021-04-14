870c7b1657ede0d56437b20131171bec132fbf33
JosePabloBonilla <2750@holbertonschool.com>
2021-04-14 14:29:07 +0000

    add path.c

---
+++ test2/path.c
@@ -0,0 +1,54 @@
+#include "header.h"
+/**
+ * _getenv - get environment variable
+ * @var: command that user uses
+ * Return: the environment variable
+ */
+char *_getenv(char *var)
+{
+	char *token;
+	int i = 0;
+
+	while (environ[i])
+	{
+		if (_strstr(environ[i], var) != 0)
+		{
+			token = _strstr(environ[i], "=");
+			if (token[0] == '=')
+				return (token);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * path - get command line and give address
+ * @comm_line: command that user uses
+ * Return: address
+ */
+char *path(char *comm_line)
+{
+	struct stat st;
+	char *path, *address, *comm;
+	char *token, *pathdup;
+
+	pathdup = _strdup(_getenv("PATH"));
+	command = str_concat("/", comm_line);
+	address = _strdup(comm_line);
+	token = strtok(pathdup, ":");
+	for (i = 0; token != NULL; i++)
+	{
+		path = str_concat(token, comm);
+		if (stat(path, &st) == 0)
+		{
+			_free(&address);
+			address = _strdup(path);
+		}
+		token = strtok(NULL, ":");
+		_free(&path);
+	}
+	_free(pathdup);
+	_free(&comm);
+	return (address);
+}
